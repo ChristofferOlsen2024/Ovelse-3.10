42a84f83dc4790d7bb97964f736d3b5d30ec7005
Christoffer Olsen <chris70944@gmail.com>
2024-03-13 23:30:56 +0100

    Add project files.

---
+++ Ovelse 3.10/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hvad er prisen på varen?");
+
+double belob = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Prisen er {belob:C}");
